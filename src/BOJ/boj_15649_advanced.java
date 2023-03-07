2d6aca7f67d94333d2136a30f13a98463ffaeb29
HamJeeHyum <2075jh@gmail.com>
2023-03-07 19:21:11 +0900

    boj_15649_advanced

---
+++ src/BOJ/boj_15649_advanced.java
@@ -0,0 +1,67 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_15649_advanced {
+    static StringBuilder sb = new StringBuilder();
+    static int N, M;
+    static int[] selected, used;
+
+    static void input() {
+        FastReader scan = new FastReader();
+        N = scan.nextInt();
+        M = scan.nextInt();
+        selected = new int[M + 1];
+        used = new int[N + 1];
+    }
+
+    static void rec_func(int k) {
+        if (k == M + 1) {
+            for (int i=1; i<=M ; i++) sb.append(selected[i]).append(' ');
+            sb.append('\n');
+        } else {
+            for (int cand=1; cand <= N ; cand ++) {
+                if (used[cand] == 1) continue;
+
+                selected[k] = cand;
+                used[cand] = 1;
+
+                rec_func(k+1);
+
+                selected[k] = 0;
+                used[cand] = 0;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        input();
+
+        rec_func(1);
+        System.out.println(sb.toString());
+    }
+
+
+    static class FastReader {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
