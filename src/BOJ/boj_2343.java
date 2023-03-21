6c21e9abbc0f071ba8bc684c73b02849cbbe158c
HamJeeHyum <2075jh@gmail.com>
2023-03-21 23:39:52 +0900

    boj_2343

---
+++ src/BOJ/boj_2343.java
@@ -0,0 +1,93 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class boj_2343 {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static int N, M;
+    static int[] A;
+
+    static void input() {
+        N = scan.nextInt();
+        M = scan.nextInt();
+        A = new int[N + 1];
+        for (int i = 1; i <= N; i++) A[i] = scan.nextInt();
+    }
+
+    static boolean determination(int len) {
+        int cnt = 1; // 사용되는 블루레이 수
+        int sum = 0; // 블루레이에 저장되는 영상의 길이 합
+
+        for (int i = 1; i <= N; i++) {
+            if (sum + A[i] > len) {
+                cnt++;
+                sum = A[i];
+            } else {
+                sum += A[i];
+            }
+        }
+        return cnt <= M;
+    }
+
+    static void pro() {
+        Arrays.sort(A, 1, N + 1);
+        int L = A[N], R = 1000000000;
+        int ans = 0;
+
+        while (L <= R) {
+            int mid = (L + R) / 2;
+            if (determination(mid)) {
+                ans = mid;
+                R = mid - 1;
+            } else {
+                L = mid + 1;
+            }
+        }
+        System.out.println(ans);
+    }
+
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
