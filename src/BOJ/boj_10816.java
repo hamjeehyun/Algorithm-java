091195cdd7296def9a3882e085ebb514e3d3b727
HamJeeHyum <2075jh@gmail.com>
2023-03-21 17:56:19 +0900

    boj_10816

---
+++ src/BOJ/boj_10816.java
@@ -0,0 +1,103 @@
+package BOJ;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class boj_10816 {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static int N;
+    static int[] A;
+
+    static void input() {
+        N = scan.nextInt();
+        A = new int[N + 1];
+        for (int i = 1; i <= N; i++) A[i] = scan.nextInt();
+    }
+
+    static int lower_bound(int[] A, int L, int R, int X) {
+        int ans = R + 1;
+        while (L <= R) {
+            int mid = (L + R) / 2;
+
+            if (A[mid] >= X) {
+                ans = mid;
+                R = mid - 1;
+            } else {
+                L = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    static int upper_bound(int[] A, int L, int R, int X) {
+        int ans = R + 1;
+        while (L <= R) {
+            int mid = (L + R) / 2;
+
+            if (A[mid] > X) {
+                ans = mid;
+                R = mid - 1;
+            } else {
+                L = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+    static void pro() {
+        Arrays.sort(A, 1, N + 1);
+        int M = scan.nextInt();
+        for (int i=1; i <= M; i++) {
+            int X = scan.nextInt();
+            int upper = upper_bound(A, 1, N, X);
+            int lower = lower_bound(A, 1, N, X);
+
+            sb.append(upper-lower).append(' ');
+        }
+        System.out.println(sb);
+    }
+
+    public static void main(String[] args) {
+        input();
+        pro();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
