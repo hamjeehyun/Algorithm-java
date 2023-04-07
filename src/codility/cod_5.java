0298e02cdde34842bf47e6ff420291b400aae84b
HamJeeHyum <2075jh@gmail.com>
2023-04-07 13:57:41 +0900

    cod_5

---
+++ src/codility/cod_5.java
@@ -0,0 +1,68 @@
+package codility;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class cod_5 {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static int[] A = new int[4];
+
+    static void input() {
+        for (int i = 0; i < 4; i++)
+            A[i] = scan.nextInt();
+    }
+
+    static int pro() {
+        Arrays.sort(A);
+
+        for (int i = 0; i < A.length; i++) {
+            if (i + 1 != A[i]) return i + 1;
+        }
+
+        return A.length + 1;
+    }
+
+    public static void main(String[] args) {
+        input();
+        System.out.println(pro());
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
