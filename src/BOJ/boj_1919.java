62ccb19af9554a01ef845356957696b917dffbe8
HamJeeHyum <2075jh@gmail.com>
2023-04-11 17:48:04 +0900

    boj_1919

---
+++ src/BOJ/boj_1919.java
@@ -0,0 +1,30 @@
+package BOJ;
+
+import java.util.Scanner;
+
+public class boj_1919 {
+    static int[] getCount(String str) {
+        int count[] = new int[26];
+        int[] countB = new int[26];
+
+        for (int i = 0; i < str.length(); i++)
+            count[str.charAt(i) - 'a']++;
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // 없애야만 하는 문자 : 공통 문자를 제외한 부분
+        String a = sc.next();
+        String b = sc.next();
+
+        int[] countA = getCount(a);
+        int[] countB = getCount(b);
+
+        int ans = 0;
+        for (int i = 0; i < 26; i++)
+            ans += Math.abs(countA[i] - countB[i]);
+        System.out.println(ans);
+    }
+}
