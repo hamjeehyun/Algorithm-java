dbfe602e194bb7af4d0898efbc901c872b069035
HamJeeHyum <2075jh@gmail.com>
2023-04-13 22:09:35 +0900

    boj_1920

---
+++ src/BOJ_review/boj_1920.java
@@ -0,0 +1,89 @@
+package BOJ_review;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class boj_1920 {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static int N;
+    static int[] A;
+
+    static void input() {
+        N = scan.nextInt();
+        A = new int[N + 1];
+        for (int i = 1; i <= N; i++) A[i] = scan.nextInt();
+    }
+
+    static boolean binarySearch(int L, int R, int target) {
+        while (L <= R) {
+            int mid = (L + R) / 2;
+
+            if (A[mid] == target) return true;
+            else if (A[mid] < target) {
+                L = mid + 1;
+            } else {
+                R = mid - 1;
+            }
+        }
+
+        return false;
+    }
+
+    static void solution() {
+        int M = scan.nextInt();
+
+        Arrays.sort(A, 1, N + 1);
+
+        for (int i = 1; i <= M; i++) {
+            int X = scan.nextInt();
+            boolean res = binarySearch(1, N, X);
+            if (res) sb.append(1).append('\n');
+            else sb.append(0).append('\n');
+        }
+        System.out.println(sb);
+    }
+
+    public static void main(String[] args) {
+        input();
+        solution();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
