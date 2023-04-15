b74c3f41a5d4931ae97570c7ddac6ede1184b435
HamJeeHyum <2075jh@gmail.com>
2023-04-15 20:11:52 +0900

    boj_13144

---
+++ src/BOJ_review/boj_13144.java
@@ -0,0 +1,79 @@
+package BOJ_review;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj_13144 {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static int N;
+    static int[] A, cnt;
+
+    static void input() {
+        N = scan.nextInt();
+        A = new int[N + 1];
+        for (int i = 1; i <= N; i++)
+            A[i] = scan.nextInt();
+        cnt = new int[100001];
+    }
+
+    static void solution() {
+        int R = 0;
+        long ans = 0;
+
+        for (int L = 1; L <= N; L++) {
+            while (R + 1 <= N && cnt[A[R + 1]] == 0) {
+                R++;
+                cnt[A[R]]++;
+            }
+
+            ans += R - L + 1;
+
+            cnt[A[L]]--;
+        }
+
+        System.out.println(ans);
+    }
+
+    public static void main(String[] args) {
+        input();
+        solution();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
