3bd0a14028c765ea26526c320c667726ead6be41
HamJeeHyum <2075jh@gmail.com>
2023-04-15 19:47:14 +0900

    boj_2230

---
+++ src/BOJ_review/boj_2230.java
@@ -0,0 +1,76 @@
+package BOJ_review;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class boj_2230 {
+    static FastReader scan = new FastReader();
+    static StringBuilder sb = new StringBuilder();
+
+    static int N, M;
+    static int[] A;
+
+    static void input() {
+        N = scan.nextInt();
+        M = scan.nextInt();
+        A = new int[N + 1];
+        for (int i = 1; i < N + 1; i++) A[i] = scan.nextInt();
+    }
+
+
+    static void solution() {
+        Arrays.sort(A, 1, N + 1);
+        int ans = Integer.MAX_VALUE, R = 1;
+
+        for (int L = 1; L <= N; L++) {
+            while (R + 1 <= N && A[R] - A[L] < M) {
+                ++R;
+            }
+
+            if(A[R] - A[L] >= M) ans = Math.min(ans, A[R] - A[L]);
+        }
+        System.out.println(ans);
+    }
+
+    public static void main(String[] args) {
+        input();
+        solution();
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
